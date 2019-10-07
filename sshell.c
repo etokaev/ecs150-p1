ef41308a73cca01feb13a58d40d54193e37f864d
vmatsius <vmatsius@ucdavis.edu>
2019-10-07 12:40:31 -0700

    phase2 exit loop

--- sshell.c
+++ sshell.c
@@ -4,13 +4,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-/*
+
 int display_prompt(){
 	
 	printf("sshell$ ");
 	return 0;
 }
 
+/*
 int read_command(char** command){
 	
 	return 0;
@@ -18,24 +19,27 @@ int read_command(char** command){
 */
 int main(int argc, char *argv[])  //first line comment//
 {
-	char **command	= NULL;
+	//char **command;
 	int status = 0;
 	pid_t pid;
 	while(1){
-			//display_prompt();
+			display_prompt();
 			//read_command(command);
 			
 			pid = fork(); 	
 			if (pid != 0){
 				/*Parent*/
 				waitpid(-1, &status, 0);
-				printf("inside parent ");
+				printf("inside parent \n");
+				fprintf(stderr, "+ completed '%s' [%d]\n", "ls", status);
+				exit(0);
 			} else { //FIND OUT HOW TO ENTER THIS BRANCH!
 				printf("inside child");	
-				execv(command[0],command);
+				execlp("ls", "ls", (char *)NULL);
+				//execv(command[0],command);
 				perror("execv");
 				exit(1);
-				fprintf(stderr, "+ completed '%s' [%d]\n", command[0], status);
+				//fprintf(stderr, "+ completed '%s' [%d]\n", "ls", status);
 			}
 	}
 	
