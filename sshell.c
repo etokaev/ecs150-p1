4228ab34515362e3dff15fb73d2015c751a112b2
etokaev <etokaev@ad3.ucdavis.edu@pc32.cs.ucdavis.edu>
2019-10-06 18:44:55 -0700

    draft

--- sshell.c
+++ sshell.c
@@ -4,30 +4,40 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+/*
+int display_prompt(){
+	
+	printf("sshell$ ");
+	return 0;
+}
+
+int read_command(char** command){
+	
+	return 0;
+}
+*/
 int main(int argc, char *argv[])  //first line comment//
 {
+	char **command	= NULL;
+	int status = 0;
 	pid_t pid;
-	char *cmd[4] ={"/bin/date","-u",NULL};
-	//int retval;
-	int status;
-        pid = fork(); 	
-        if (pid == 0){
-		/*Child*/
-		execvp(cmd[0],cmd);
-		perror("execvp");
-		exit(1);
-	} else if (pid>0){
-		/*Parent*/
-		waitpid(-1, &status, 0);
-		//printif("Child exited with status: %d\n", WEXITSTATUS(status));
-		fprintf(stderr, "+ completed '%s' [%d]\n", cmd[0], status);
-	} else {
-		perror("fork");
-		exit(1);
-
+	while(1){
+			//display_prompt();
+			//read_command(command);
+			
+			pid = fork(); 	
+			if (pid != 0){
+				/*Parent*/
+				waitpid(-1, &status, 0);
+				printf("inside parent ");
+			} else { //FIND OUT HOW TO ENTER THIS BRANCH!
+				printf("inside child");	
+				execv(command[0],command);
+				perror("execv");
+				exit(1);
+				fprintf(stderr, "+ completed '%s' [%d]\n", command[0], status);
+			}
 	}
-	//retval = system(cmd);
-        //fprintf(stdout, "Return status value for '%s': %d\n", cmd[0], retval);
-
-	return EXIT_SUCCESS;
+	
+  	return EXIT_SUCCESS;
 }
