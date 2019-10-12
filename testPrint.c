bfe957f660dccb75e222d44061d873217403335b
etokaev <etokaev@ad3.ucdavis.edu@pc32.cs.ucdavis.edu>
2019-10-11 17:37:33 -0700

    AUTHORS

---
+++ testPrint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+
+	char* lineInput;
+        lineInput = (char*)malloc(512 + 1);
+        fgets(513,stdin);
+        printf("%s",lineInput);
+        return 0; 
+
+
+
+
+}
